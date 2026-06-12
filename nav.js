439c3a4c64ec379e2eee52acd9522380f9fdfc15
Ravshan Rakhmanov <ravshanrakhmanov@iMac.localdomain>
2026-06-12 07:27:58 -0400

    COT FX page: 3-group nets + Williams COT Index w/ 95/5 bands + extremes board + USD crowding aggregate; nav entry

--- nav.js
+++ nav.js
@@ -13,6 +13,7 @@
     ['cor1m_contango.html',        '▼ Tail Hedge'],
     ['cyclicality.html',           '◐ CycleLab'],
     ['gex.html',                   'Γ GEX'],
+    ['cot.html',                   '⚖ COT FX'],
   ];
   function build() {
     const here = (location.pathname.split('/').pop() || 'index.html');
